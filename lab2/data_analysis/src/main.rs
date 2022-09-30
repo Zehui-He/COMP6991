1ea3ce95fb681eba953bf6d4b90f4ca76af616ed
Zehui-He <92138586+Zehui-He@users.noreply.github.com>
2022-09-30 22:55:39 +1000

    lab2 updated last question

--- lab2/data_analysis/src/main.rs
+++ lab2/data_analysis/src/main.rs
@@ -3,18 +3,17 @@ use std::{collections::{HashMap, HashSet}, io::{stdin, stdout, Write}};
 use csv::{ReaderBuilder};
 use serde::Deserialize;
 
-#[allow(dead_code)]
-#[derive(Deserialize, Debug)]
-struct Row<'a> {
-    course_code: &'a str,
+#[derive(Deserialize, Debug, Clone)]
+struct Row {
+    course_code: String,
     student_id: u32,
-    name: &'a str,
-    program: &'a str,
-    plan: &'a str,
+    name: String,
+    program: String,
+    plan: String,
     wam: f32,
-    session: &'a str,
-    bday: &'a str,
-    sex: &'a str,
+    session: String,
+    bday: String,
+    sex: String,
 }
 
 fn main() {
@@ -24,44 +23,36 @@ fn main() {
         .from_path(ENROLMENTS_PATH)
         .unwrap();
 
-    let mut record_vec = Vec::new();
-    for result in rdr.records() {
-        let record = result.unwrap();
-        record_vec.push(record);
+    let enrolments = rdr.deserialize::<Row>()
+        .filter_map(|result| result.ok())
+        .collect::<Vec<Row>>();
+
+    // Find all stduent
+    let mut students = HashSet::new();
+    for enrolment in enrolments.iter() {
+        students.insert(enrolment.student_id);
     }
+    println!("Number of students: {}", students.len());
 
-    let mut seen_id = HashSet::new();
+    // Find the most and least comment course
     let mut course_student: HashMap<String, HashSet<u32>> = HashMap::new();
-    let mut cse_grade: HashMap<u32, f32> = HashMap::new();
-    record_vec.iter().for_each(|record| {
-        let row = record.deserialize::<Row>(None).unwrap();
-
-        // Count the number of student
-        seen_id.insert(row.student_id);
-
-        // Record which student enrolled in which course
-        course_student.entry(row.course_code.to_string())
-            .and_modify(|student_list| { student_list.insert(row.student_id); })
-            .or_insert(HashSet::from([row.student_id]));
-
-        // Record score for each cse stduent
-        cse_grade.insert(row.student_id, row.wam);
+    enrolments.iter().for_each(|enrolment| {
+        course_student.entry(enrolment.course_code.clone())
+            .and_modify(|set| { set.insert(enrolment.student_id); })
+            .or_insert(HashSet::from([enrolment.student_id]));
     });
+    let (max_course, max_studnet) = course_student.iter().max_by_key(|(_, x)| {x.len()}).unwrap();
+    let (min_course, min_studnet) = course_student.iter().min_by_key(|(_, x)| {x.len()}).unwrap();
+    println!("Most common course: {} with {} students", max_course, max_studnet.len());
+    println!("Least common course: {} with {} students", min_course, min_studnet.len());
 
-    // Find max course and min course
-    let (max_course_name, max_studnet) = course_student.iter()
-        .max_by_key(|(_course, student)| student
-        .len()).unwrap();
-    let (min_course_name, min_studnet) = course_student.iter()
-        .min_by_key(|(_course, student)| student
-        .len()).unwrap();
+    // Calculate the wam of all students
+    let grade:HashMap<u32, f32> = enrolments.iter().map(|enrolment| {
+        (enrolment.student_id, enrolment.wam)
+    }).collect();
+    println!("Average WAM: {:.2}", grade.values().sum::<f32>() / grade.len() as f32);
 
-    // Print the number of studnets
-    println!("Number of students: {}", seen_id.len());
-    println!("Most common course: {} with {} students", max_course_name, max_studnet.len());
-    println!("Least common course: {} with {} students", min_course_name, min_studnet.len());
-    println!("Average WAM: {:.2}", cse_grade.values().sum::<f32>() / cse_grade.len() as f32);
-
+    // Search for the stduent
     let mut input_string = String::new();
     print!("Enter a student id: ");
     // Remove the newline
@@ -72,22 +63,32 @@ fn main() {
         Ok(id) => id,
         Err(_) => {
             println!("== NOT FOUND ==");
-            std::process::exit(0);
+            return;
         }
     };
-    record_vec.iter().for_each(|record| {
-        let row = record.deserialize::<Row>(None).unwrap();
-        if target_id == row.student_id {
-            println!("== STUDENT FOUND ==");
-            println!("Name: {}", row.name);
-            println!("Program: {}", row.program);
-            println!("Plan: {}", row.plan);
-            println!("WAM: {:.2}", row.wam);
-            println!("Session: {}", row.session);
-            println!("Birthdate: {}", row.bday);
-            println!("Sex: {}", row.sex);
-            std::process::exit(0);
+    let target =  match find_stduent(target_id, &enrolments) {
+        Some(enrolment) => enrolment,
+        None => {
+            println!("== NOT FOUND ==");
+            return;
         }
-    });
-    println!("== NOT FOUND ==");
+    };
+
+    println!("== STUDENT FOUND ==");
+    println!("Name: {}", target.name);
+    println!("Program: {}", target.program);
+    println!("Plan: {}", target.plan);
+    println!("WAM: {:.2}", target.wam);
+    println!("Session: {}", target.session);
+    println!("Birthdate: {}", target.bday);
+    println!("Sex: {}", target.sex);
+}
+
+fn find_stduent(target_id: u32, enrolments: &Vec<Row>) -> Option<&Row> {
+    for enrolment in enrolments {
+        if target_id == enrolment.student_id {
+            return Some(enrolment);
+        }
+    }
+    return None;
 }
