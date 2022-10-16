39215a6d07c1a3a1ff23971068fff0c2350c6763
Zehui He <hoxzaffi@gmail.com>
2022-10-16 22:26:03 +1100

    added assign1 starter code

---
+++ adventurers/adventurers/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Welcome to AdventureRS");
+    println!("To get started, you should read the termgame documentation,");
+    println!("and try out getting a termgame UI to appear on your terminal.");
+}
